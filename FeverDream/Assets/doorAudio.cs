ca0e1e3206bdbaeb61e4f6ee8419f7eae4101fe2
mikey <mikewoudstra888@gmail.com>
2021-06-07 13:57:43 +0200

    audio done

---
+++ FeverDream/Assets/doorAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public FMODUnity.StudioEventEmitter open;
+    [SerializeField]
+    public FMODUnity.StudioEventEmitter close;
+    
+
+    public void Open()
+    {
+        open.Play();
+    }
+
+    public void Close()
+    {
+        close.Play();
+    }
+}
